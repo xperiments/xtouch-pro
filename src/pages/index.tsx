5e15b5431a2b47c68c1d5fd17ecc4e39e939511d
Pedro Casaubon <xperiments@gmail.com>
2025-04-28 22:17:18 +0200

    Added supporters

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Heading from "@theme/Heading";
 
 import styles from "./index.module.css";
+import Supporters from "../components/Supporters";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -134,6 +135,7 @@ export default function Home(): JSX.Element {
           </div>
         </div>
       </main>
+      <Supporters />
     </Layout>
   );
 }
